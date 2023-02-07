1fa5ae369dc4287abeec747c1caf7d1b45ecb4ee
Naresh_mateti <122420123+matetinaresh@users.noreply.github.com>
2023-02-07 07:44:33 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -30,7 +30,7 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "sonarqube analysis with in jenkins is succeded:success" ;
+        public String ping() {  return "backend server deploy success" ;
         			
         }
 
